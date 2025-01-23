c5c9b26823caf80a9e45bcdbd0b494722d9fb579
Andre Nunes <andrenunes.work@gmail.com>
2025-01-23 00:27:23 +0000

    Add NextJS auth

---
+++ app/login/page.tsx
@@ -0,0 +1,65 @@
+import { login } from "@/utils/supabase/actions"
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
+import Image from "next/image"
+
+export default async function LoginPage() {
+  const supabase = await createClient()
+
+  const { data } = await supabase.auth.getUser()
+  if (data?.user) {
+    redirect("/home")
+  }
+
+  return (
+    <div className="w-screen">
+      <div className="animate-in pt-24 px-4 max-w-md m-auto">
+        <Image
+          alt="fran-methodology"
+          src="/fran-methodology-logo.png"
+          width={448}
+          height={448}
+        />
+
+        <form className="max-w-sm">
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            className="border-2 py-2 px-3 rounded-md mb-4 w-full"
+          />
+
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            className="border-2 py-2 px-3 rounded-md w-full"
+          />
+
+          <button
+            formAction={login}
+            className="mt-6 w-full bg-projectGreen text-white py-1 rounded-md text-lg tracking-wide transition duration-1000"
+          >
+            Log in
+          </button>
+        </form>
+
+        <div className="flex mt-8">
+          <span className="block items-center text-gray-700 font-medium text-sm text-center">
+            Don&apos;t have an account?
+            <a
+              href="https://franpadelproject.com/contact"
+              className="ml-2 text-projectGreen font-semibold"
+            >
+              Contact us
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
@@ -9,10 +9,31 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        projectBlue: "#122C49",
+        projectGreen: "#6bb8a4",
+        projectGray: "#F4F4F4",
+        green: {
+          50: "#f3faf7",
+          100: "#d7f0e8",
+          200: "#afe0d0",
+          300: "#7fc9b4",
+          400: "#6bb8a4",
+          500: "#3a927c",
+          600: "#2d7464",
+          700: "#275e53",
+          800: "#234c44",
+          900: "#21403a",
+          950: "#0e2521",
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        btn: {
+          background: "hsl(var(--btn-background))",
+          "background-hover": "hsl(var(--btn-background-hover))",
+        },
+        dimmed: "hsl(var(--dimmed))",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
