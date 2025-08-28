13d7b48620da6bab6f3289cd7c6fc7f8cd1fae45
Andre Nunes <andrenunes.work@gmail.com>
2025-08-28 13:29:33 +0100

    Refactor folders to 'src' style

---
+++ src/lib/content-manager.js
@@ -0,0 +1,448 @@
+// This file contains questions data for the certification system
+// The contentful-management client setup is commented out as it's not currently used
+
+export const QUESTIONS = [
+  {
+    id: 'i08pe4RZE35ImaibCyQQFH',
+    number: 1,
+    question: 'O conceito "5 em linha" segue uma ordem conceptual:',
+    answers: [
+      'Postura, preparação, mobilidade, estabilidade e ponto de impacto',
+      'Postura, preparação, estabilidade, mobilidade e ponto de impacto',
+      'Postura, estabilidade, preparação, mobilidade e ponto de impacto',
+      'Nenhuma das respostas anteriores.',
+    ],
+  },
+  {
+    id: 'qmjyQXHuNkdqQNk6w7HbYs',
+    number: 2,
+    question: 'Quais os diferentes tipos de posturas que existem dentro da modalidade:',
+    answers: ['3', '2', '1', '4'],
+  },
+  {
+    id: 'vpSrM5Z4yGrcIBZPAbpRNN',
+    number: 3,
+    question: 'Qual a postura mais recomendada na zona defensiva dentro das seguintes opções:',
+    answers: ['"Chingoto"', '"Paquito"', '"Bela"', '"Arigato"'],
+  },
+  {
+    id: 'r86XwlI4dJg8m0TMaaHmnK',
+    number: 4,
+    question: 'Quais os contras da postura "Chingoto":',
+    answers: [
+      'Sobrecarga dos quadríceps',
+      'Sobrecarga das lombrares',
+      'Sobrecarga dos gluteos',
+      'Sobrecarga dos quadríceps e gluteos',
+    ],
+  },
+  {
+    id: '02VKSgAgSCIVHQzICh0doH',
+    number: 5,
+    question: 'Qual a pega mais recomendada para se jogar padel:',
+    answers: ['Continental', 'Direita', 'Esquerda', 'Todas as anteriores'],
+  },
+  {
+    id: 'RilLFJZldBC1CaIr9u2lT9',
+    number: 6,
+    question: 'Quantos pontos de impacto existem no padel:',
+    answers: ['4', '3', '2', '5'],
+  },
+  {
+    id: 'scxTP2oW0GXvAf4cOLn0Aa',
+    number: 7,
+    question:
+      'Os pontos de impacto estão associados a direções. Indique a correspondência mais indicada para um jogador de esquerda que execute uma direita sem vidro:',
+    answers: [
+      'Paralelo =» Ponto de impacto à frente do corpo / Meio =» Ponto de impacto alinhado com o ombro da frente / Cruzado =» Ponto de impacto entre os ombros / Malha =» Ponto de impacto atrás do corpo',
+      'Paralelo =» Ponto de impacto atrás do corpo / Meio =» Ponto de impacto alinhado com o ombro da frente / Cruzado =» Ponto de impacto entre os ombros / Malha =» Ponto de impacto à frente do corpo',
+      'Paralelo =» Ponto de impacto entre os ombros / Meio =» Ponto de impacto alinhado com o ombro da frente / Cruzado =» Ponto de impacto a frente do corpo / Malha =» Ponto de impacto totalmente à frente do corpo',
+      'Nenhuma das anteriores.',
+    ],
+  },
+  {
+    id: 'IdfhoDHpVH3yjSzFcdio7a',
+    number: 8,
+    question:
+      'Os pontos de impacto estão associados a direções. Indique a correspondência mais indicada para um jogador de direita que execute uma direita sem vidro:',
+    answers: [
+      'Paralelo =» Ponto de impacto entre os ombros/ Meio =» Ponto de impacto alinhado com o ombro da frente / Cruzado =» Ponto de impacto à frente do corpo / Malha =» Ponto de impacto totalmente à frente do corpo.',
+      'Paralelo =» Ponto de impacto entre os ombros/ Meio =» Ponto de impacto alinhado com o ombro da frente / Cruzado =» Ponto de impacto totalmente frente do corpo / Malha =» Ponto de impacto à frente do corpo',
+      'Paralelo =» Ponto de impacto entre os ombros/ Meio =» Ponto de impacto totalmente à frente do corpo / Cruzado =» Ponto de impacto totalmente frente do corpo / Malha =» alinhado com o ombro da frente',
+      'Nenhuma das anteriores.',
+    ],
+  },
+  {
+    id: 'EM3GvPdvE0fWph1SC7QliX',
+    number: 9,
+    question: 'A estabilidade no conceito "5 em linha" é influenciada de modo negativo pelos seguintes fatores:',
+    answers: [
+      'Distância dos pés à largura dos ombros e instabilidade corporal',
+      'Mobilidade excessiva',
+      'Mobilidade excessiva e instabilidade corporal',
+      'Todas as respostas anteriores',
+    ],
+  },
+  {
+    id: 'sDyxJLexEkQNWZbFtdJeV3',
+    number: 10,
+    question: 'O "mind set" da preparação da mecânica de um movimento técnico deve ser:',
+    answers: [
+      'Diretamente proporcional à velocidade da bola',
+      'Inversamente proporcional à velocidade da bola',
+      'Todas as respostas anteriores',
+      'Nenhuma das respostas anteriores',
+    ],
+  },
+  {
+    id: 'KYiq1y7fwxffSx0hyqh058',
+    number: 11,
+    question: 'A velocidade da bola é medida dentro de uma escala de:',
+    answers: ['0 – 10', '0 – 5', '5 – 10', '1 – 5'],
+  },
+  {
+    id: 'NFXxUElXlnXYT1doEymA6X',
+    number: 12,
+    question: 'Cada velocidade tem uma caraterização especifica:',
+    answers: [
+      'V1 – 3 toques até ao vidro; V2 – 2 até ao vidro; V3 - 1 toque até ao vidro e ressalto à linha de fundo; V4 – 1 toque até ao vidro; V5 – 1 toque até ao vidro e ultrapassa largamente a linha branca após ressaltar no vidro de fundo.',
+      'V1 – 1 toque até ao vidro; V2 – 2 até ao vidro; V3 - 3 toque até ao vidro e ressalto à linha de fundo; V4 – 1 toque até ao vidro; V5 – 1 toque até ao vidro e ultrapassa largamente a linha branca após ressaltar no vidro de fundo.',
+      'V1 – 2 toques até ao vidro; V2 – 3 até ao vidro; V3 - 1 toque até ao vidro e ressalto à linha de fundo; V4 – 1 toque até ao vidro; V5 – 1 toque até ao vidro e ultrapassa largamente a linha branca após ressaltar no vidro de fundo.',
+      'Nenhuma das respostas anteriores.',
+    ],
+  },
+  {
+    id: 'vlq4JNL5BKohutb3mFjLYj',
+    number: 13,
+    question: 'Quais os aspetos mais relevantes no controlo da velocidade de bola:',
+    answers: [
+      'A velocidade com que a mão passa pela bola e a chegada do corpo à bola',
+      'A amplitude da preparação',
+      'Gerar a maior quantidade possível de tensão muscular',
+      'Todas as respostas anteriores.',
+    ],
+  },
+  {
+    id: 'MoqugkoKtdlqXV0uYY4RQd',
+    number: 14,
+    question: 'Quando abordamos a força faz sentido em falar aceleração?',
+    answers: [
+      'Sim, F= (m. a)',
+      'Não',
+      'Não. A força é um conceito sem qualquer relação com a aceleração.',
+      'Sim, F= (m. a) / t, e a= (vf – vi) /t',
+    ],
+  },
+  {
+    id: 'aYEaI8QlYaP7HCbs2tiGFY',
+    number: 15,
+    question: 'Quais os erros mais característicos da instabilidade corporal dos membros superiores:',
+    answers: [
+      'Descarga elétrica à modo CR7',
+      'Descarga elétrica em modo águia',
+      'Descarga elétrica em modo susto',
+      'Todas as respostas anteriores',
+    ],
+  },
+  {
+    id: 'dMTZNR24bEkcE4Eo78AQeq',
+    number: 16,
+    question: 'A altura da bola é uma variável possível de controlar através dos seguintes aspetos:',
+    answers: ['Cara da raquete', 'Ponta da raquete', 'Instabilidade corporal', 'Todas as anteriores.'],
+  },
+  {
+    id: 'eVQ1vJBOcjf4hIAADe0f3W',
+    number: 17,
+    question: 'A teoria do magnetismo refere que os braços seguem um padrão de movimento:',
+    answers: [
+      'No jogador destro, o braço esquerdo funciona como o íman mais forte que atrai o íman mais fraco, o braço direito, então a coordenação deve ser controlada e orientada',
+      'No jogador esquerdino, o braço esquerdo funciona como o íman mais fraco que atrai braço direito, o íman mais forte, então a coordenação deve ser controlada e orientada',
+      'Implica que na lateralidade dominante, os jogadores apresentam uma terminação dos movimentos biomecânicos caracterizada pela junção das mãos',
+      'Todas as opções anteriores',
+    ],
+  },
+  {
+    id: 'SoO8anNvxJUo6o5HQNzbA9',
+    number: 18,
+    question: 'Na postura ao estilo "Paquito" existem vantagens e desvantagens associados:',
+    answers: [
+      'A utilização das lombares é uma desvantagem',
+      'Uma maior necessidade de utilização da mão é uma desvantagem',
+      'A base de apoio mais pequena é uma desvantagem por potenciar uma maior instabilidade',
+      'Nenhuma das opções anteriores.',
+    ],
+  },
+  {
+    id: 'oeANqr0wudRCNVTagSOb24',
+    number: 19,
+    question: 'Todos os jogadores apresentam uma maior rotação a nível anatómico numa das lateralidades corporais:',
+    answers: [
+      'Um destro apresenta no lado direito',
+      'Um esquerdino apresenta no lado esquerdo',
+      'Opções a) e b) estão corretas',
+      'Nenhuma nas opções anteriores.',
+    ],
+  },
+  {
+    id: 'FbRnNt1mRiNhCBKGZQwG1Z',
+    number: 20,
+    question: 'A posição da raquete nas polaridades positiva e neutra deve estar posicionada a:',
+    answers: ['85º', '75º', '65º', '45º'],
+  },
+  {
+    id: 'PPw2pEbZTiYfssquGVtpEK',
+    number: 21,
+    question: 'A preparação para a execução de um volley deve seguir a ordem:',
+    answers: [
+      'Enquadramento da cara da raquete com a bola, desbloqueio da cintura, avançar a raquete e a perna diretora ao mesmo tempo',
+      'Enquadramento da cara da raquete com a bola, desbloqueio da cintura, avançar a raquete depois da perna diretora',
+      'Desbloqueio da cintura, enquadramento da cara da raquete com a bola, avançar a raquete primeiro, e depois a perna diretora',
+      'Desbloqueio da cintura, enquadramento da cara da raquete com a bola, avançar a raquete e a perna diretora ao mesmo tempo',
+    ],
+  },
+  {
+    id: 'AAQDUTv9W5hfNnQU4UIsV4',
+    number: 22,
+    question: 'Os volley podem ser de diferentes tipo que correspondem às seguintes polaridades:',
+    answers: ['++ / = / - / --/ +', '+ / = / -', '++/ = / --', '+ / = / - / -- /'],
+  },
+  {
+    id: 'UoVmpbpvxGz24dbijlXV2u',
+    number: 23,
+    question: 'Quais são os efeitos primordiais no padel:',
+    answers: [
+      'Plano, Cortado, Top-Sin e Side-Spin',
+      'Plano, Cortado e Side-Spin',
+      'Plano, Cortado e Top-Spin',
+      'Nenhuma das opções anteriores',
+    ],
+  },
+  {
+    id: 'WRCCODu8MY9GwpcGqy1VWr',
+    number: 24,
+    question: 'A zona de conforto de impacto é compreendida entre:',
+    answers: ['Cintura e bacia', 'Cintura e peitoral', 'Bacia e Joelho', 'Cintura e joelho'],
+  },
+  {
+    id: 'Sa1A0AHtjw8ufXdcGDINNn',
+    number: 25,
+    question: 'A trajetória da bola é descrita por três fases:',
+    answers: [
+      'Ascendente, altura máxima e descendente',
+      'Descendente, altura máxima e ascendente',
+      'Nenhuma das opções anteriores',
+      'Opção a) e b)',
+    ],
+  },
+  {
+    id: 'QFMJuRDOtpNv1h3eNMsX3p',
+    number: 26,
+    question: 'Os padrões de movimentos técnicos podem ser:',
+    answers: ['Descendentes (+» -)', 'Ascendentes (-» +)', 'Retilíneos (=» =)', 'Todas as opções anteriores'],
+  },
+  {
+    id: 'iPadezTFeSCxcetLiQwqG8',
+    number: 27,
+    question: 'A preparação de uma direita ou esquerda sem vidro é facilmente identificada pela:',
+    answers: [
+      'Ponta da raquete a apontar para o vidro de fundo',
+      'Cara da raquete a apontar para o VL1',
+      'As opções a) e b) estão corretas',
+      'Nenhuma das opções anteriores',
+    ],
+  },
+  {
+    id: 'Wlr7SEW29xgZn9fqRmNTN8',
+    number: 28,
+    question: 'O efeito cortado consiste:',
+    answers: [
+      'Na entrada da raquete pelo centro e lado direito da bola',
+      'Na entrada da raquete pelo centro e por cima da bola',
+      'Na entrada da raquete pelo centro e debaixo da bola',
+      'Na entrada da raquete pelo centro e lado esquerdo da bola',
+    ],
+  },
+  {
+    id: 'Rz82EkqH37tZCmvQfRPyjV',
+    number: 29,
+    question: 'O efeito "Side-Spin" consiste:',
+    answers: [
+      'Na entrada da raquete pelo centro e lado direito da bola',
+      'Na entrada da raquete pelo centro e por cima da bola',
+      'Na entrada da raquete pelo centro e debaixo da bola',
+      'Na entrada da raquete pelo centro e lado esquerdo da bola',
+    ],
+  },
+  {
+    id: 'xMQdRyGMEZKsL47k1pogA6',
+    number: 30,
+    question: 'O princípio da equidade refere:',
+    answers: [
+      'A distância da cabeça à cabeça da raquete deve equivaler à de um antebraço',
+      'A distância da cabeça à cabeça da raquete deve equivaler à de um braço',
+      'A distância da cabeça à cabeça da raquete deve equivaler à de uma mão',
+      'Nenhuma das opções anteriores.',
+    ],
+  },
+  {
+    id: '0UVBZVpfsIsJ37tWdgYE7F',
+    number: 31,
+    question: 'O jogador deve ter uma relação com a bola de:',
+    answers: ['Proximidade', 'Amizade', 'Impessoal', 'Desconhecida'],
+  },
+  {
+    id: 'pmoqeXS6bzXZicz49YNxuW',
+    number: 32,
+    question: 'A nível muscular a criação de força é caracterizada pela existência de:',
+    answers: [
+      'Contração e extensão muscular',
+      'Extensão e contração muscular',
+      'Contração muscular',
+      'Extensão muscular',
+    ],
+  },
+  {
+    id: 'GykiLDe7yWmsnInrSOf2DE',
+    number: 33,
+    question: 'As mudanças de direção agressivas correspondem a:',
+    answers: ['Números ímpares', 'Números pares', 'Sectores das malhas', 'Nenhuma das opções anteriores'],
+  },
+  {
+    id: 'jybGNq4KYtQ1aJQsgYHpaL',
+    number: 34,
+    question: 'As mudanças de direção seguras correspondem a:',
+    answers: ['Números ímpares', 'Números pares', 'Sectores das malhas', 'Nenhuma das opções anteriores'],
+  },
+  {
+    id: 'O0S58YQR6ccwPVDCWeImBv',
+    number: 35,
+    question: 'A bandeja é uma técnica do jogo aéreo executada por jogadores destros:',
+    answers: [
+      'No sector horário entre as 14h00-15h00',
+      'No sector horário entre as 13h00-14h00',
+      'No sector horário entre as 12h00-13h00',
+      'Nenhuma das opções anteriores',
+    ],
+  },
+  {
+    id: 'Nq9tAtoQFS1ahtzGBjoKTr',
+    number: 36,
+    question: 'A víbora é uma técnica de jogo aéreo executada por jogadores esquerdinos:',
+    answers: [
+      'No sector horário entre as 12h00-11h00',
+      'No sector horário entre as 11h00-10h00',
+      'No sector horário entre as 09h00-10h00',
+      'Todas as opções anteriores',
+    ],
+  },
+  {
+    id: 'OLxrmIhZlqGKbUCtomtVdR',
+    number: 37,
+    question: 'A malha é uma técnica de jogo aéreo executada por jogadores destros no sentido horário:',
+    answers: ['12h00-13h00', '13h00-14h00', '15h00-16h00', '11h00-12h00'],
+  },
+  {
+    id: 'xVDjY9nUXLmj883BgL4v60',
+    number: 38,
+    question: 'O jogo aéreo é caracterizado por agrupar movimento de padrões:',
+    answers: ['Descendentes', 'Ascendentes e Retilíneos', 'Descendentes e Retilíneos', 'Nenhuma das opções anteriores'],
+  },
+  {
+    id: '0RCkEzsGUXIieyMuJ5HvZi',
+    number: 39,
+    question: 'Quantas pessoas são necessárias para jogar padel:',
+    answers: ['1', '2', '3', '4'],
+  },
+  {
+    id: 'a7FnmKgIcOpdXKDhY2VMOg',
+    number: 40,
+    question: 'A preparação da raquete no jogo aéreo apresenta uma relação:',
+    answers: [
+      'Diretamente proporcional à altura da bola',
+      'Inversamente proporcional à altura da bola',
+      'Nenhuma das opções anteriores',
+      'Opção a) e b) estão corretas',
+    ],
+  },
+  {
+    id: 'vvNsQroLWh2bQpPfrQXGGg',
+    number: 41,
+    question: 'O velocímetro corresponde a uma medida de velocidade:',
+    answers: [
+      'Orientada pelos setores da malha categorizados de modo decrescente: 3 – 2 – 1 – 0',
+      'Subjetiva categorizada de 0-5',
+      'Orientada pelos setores da malha categorizados de modo crescente: 0 – 1 - 2 – 3',
+      'Nenhuma das opções anteriores.',
+    ],
+  },
+  {
+    id: 'SsXSAK5GirSoZh3S83nwzw',
+    number: 42,
+    question: 'O sector 1 da malha corresponde:',
+    answers: ['1º Painel da malha', '1º VL', '3º Painel da malha', '2º Painel da malha'],
+  },
+  {
+    id: 'V7QUmW6WWNG6C1m1qKCRvw',
+    number: 43,
+    question: 'Existem vários tipos de transição:',
+    answers: ['1 Tempo', '2 Tempos', '3 Tempos', 'Todas as opções anteriores'],
+  },
+  {
+    id: 'fgYF2xRnt6jI4lLVGgSi6f',
+    number: 44,
+    question: 'A transição a 3 tempos é caracterizada pela seguinte sequência:',
+    answers: [
+      'Chiquita + Sector 0 + Sector 2',
+      'Balão + Sector 2 + Sector 3',
+      'Chiquita + Sector 1 + Sector 2',
+      'Nenhuma das opções anteriores',
+    ],
+  },
+  {
+    id: 'bzhGYLbDJdux40Zsy6NxOl',
+    number: 45,
+    question: 'Quantos tipos de saída de vidro existem:',
+    answers: ['Slice e plana', 'Sidespin', 'Slice, plana e sidespin', 'Todas as opções anteriores'],
+  },
+]
+
+// Commented out contentful-management setup for future use
+// const TOKEN = "CFPAT-yNA4EioO9j1nx1GT8nMAuoa4MI_Q5hcF-TC_wzVASw4";
+// const SPACE_ID = "rqt5vjnpqy42";
+// const ENVIRONMENT_ID = "master";
+// import contentful from 'contentful-management';
+// const client = contentful.createClient({ accessToken: TOKEN });
+
+// function getEntry(entry) {
+//   client.getSpace(SPACE_ID)
+//     .then(function (space) { return space.getEnvironment(ENVIRONMENT_ID); })
+//     .then(function (environment) { return environment.getEntry(entry); })
+//     .then(function (entry) { return console.log(entry); })
+//     .catch(console.error);
+// }
+
+// function createEntry(question) {
+//   client.getSpace(SPACE_ID)
+//     .then(function (space) { return space.getEnvironment(ENVIRONMENT_ID); })
+//     .then(function (environment) {
+//       return environment.createEntryWithId('question', question.id, {
+//         fields: {
+//           number: {
+//             'en-US': question.number
+//           },
+//           question: {
+//             'en-US': question.question
+//           },
+//           answers: {
+//             'en-US': question.answers
+//           }
+//         }
+//       });
+//     })
+//     .then(function (entry) { return console.log(entry); })
+//     .catch(console.error);
+// }
+
+// QUESTIONS.splice(2).map(question => {
+//   createEntry(question);
+// })
