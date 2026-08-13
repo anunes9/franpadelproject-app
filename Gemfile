e2a2a8e1d536cbb818a42c9260440cb06aee6258
Andre <a.nunes@runtime-revolution.com>
2026-08-13 17:47:39 +0100

    Store production uploads in the Railway S3-compatible bucket
    Local disk storage doesn't survive redeploys or work across replicas on
    Railway. Points ActiveStorage at the bucket via env vars instead.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
+# Active Storage's S3 service, also used to talk to the S3-compatible bucket on Railway
+gem "aws-sdk-s3", require: false
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # Store uploaded files in the Railway bucket (see config/storage.yml) -- local disk
+  # doesn't survive redeploys or work across multiple replicas on Railway.
+  config.active_storage.service = :railway
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
